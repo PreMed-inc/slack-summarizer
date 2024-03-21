5c96ae8a042f7e2f209f42fc1f2a580bf0f34649
Yuichiro MASUI <masui@masuidrive.jp>
2024-03-21 21:04:24 +0900

    Update summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -159,7 +159,7 @@ TIMEZONE_STR = str(os.environ.get('TIMEZONE') or 'Asia/Tokyo').strip()
 TEMPERATURE = float(os.environ.get('TEMPERATURE') or 0.3)
 CHAT_MODEL = str(os.environ.get('CHAT_MODEL') or "gpt-3.5-turbo").strip()
 DEBUG = str(os.environ.get('DEBUG') or "").strip() != ""
-MAX_BODY_TOKENS = 3000
+MAX_BODY_TOKENS = 100000
 
 if OPEN_AI_TOKEN == "" or SLACK_BOT_TOKEN == "" or CHANNEL_ID == "":
     print("OPEN_AI_TOKEN, SLACK_BOT_TOKEN, CHANNEL_ID must be set.")
